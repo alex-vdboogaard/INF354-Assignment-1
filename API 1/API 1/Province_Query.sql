0616f17cc6e37b7bb1f570069dd329f1eb68dd5c
Mr. A van den Boogaard <u23561302@up.ac.za>
2025-02-26 11:41:52 +0200

    rebuild API

--- API 1/API 1/Province_Query.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Provinces (Id, Code, Name, ProvinceImageURL) 
-VALUES ('b950ddf5-e9ad-47ff-9d2a-57259014fae6', 'WC', 'Western Cape Province', 'dummy-mage-url.jpg'),
-('907f54ba-2142-4719-aef9-6230c23bd7de', 'KZN', 'KwaZuluNatal Province', 'dummy1-mage-url.jpg'),
-('79e9872d-5a2f-413e-ac36-511036ccd3d4', 'GP', 'Gauteng Province', 'dummy2-mage-url.jpg'),
-('68c2ab66-c5eb-40b6-81e0-954456d06bba', 'NW', 'Northwest Province', 'dummy3-mage-url.jpg')
--- API 1/API 1/ZAHikes/ZAHikes/ZAHikes.API/Models/Domain/Difficulty.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ZAHikes.API.Models.Domain
-{
-    public class Difficulty
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-    }
-}
