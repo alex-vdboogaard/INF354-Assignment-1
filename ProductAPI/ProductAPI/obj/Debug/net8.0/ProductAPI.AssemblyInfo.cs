4cef269771864eb1f019bcf62534ce15831fd621
Mr. A van den Boogaard <u23561302@up.ac.za>
2025-02-27 09:04:34 +0200

    add classes and packages

--- ProductAPI/ProductAPI/obj/Debug/net8.0/ProductAPI.AssemblyInfo.cs
+++ ProductAPI/ProductAPI/obj/Debug/net8.0/ProductAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e819bb30e13f9adefb5bd2f90c27bd8b8566d61d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8461a85640ed208f6669553fdcc58362ae1d09a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
