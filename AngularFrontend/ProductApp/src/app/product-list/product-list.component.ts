66bd43b0eb33508d47c1f9608866afc97a7318d4
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2025-03-31 11:12:20 +0200

    add data service for list of products

--- AngularFrontend/ProductApp/src/app/product-list/product-list.component.ts
+++ AngularFrontend/ProductApp/src/app/product-list/product-list.component.ts
@@ -1,86 +1,37 @@
-import { Component } from "@angular/core";
-import { CommonModule } from "@angular/common"; // Import CommonModule for *ngFor
-import { ProductComponent } from "../product/product.component"; // Import ProductComponent for child component
-import { Product } from "../../models/product.model";
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ProductComponent } from "../product/product.component";
+import { ProductService } from "../services/product.service";
+import { Product } from "../../models/product.model"; // Make sure to import Product model
 
 @Component({
   selector: "app-product-list",
-  standalone: true, // Mark as standalone component
-  imports: [CommonModule, ProductComponent], // Import necessary modules
+  standalone: true,
+  imports: [CommonModule, ProductComponent],
   templateUrl: "./product-list.component.html",
-  styleUrls: ["./product-list.component.css"], // Corrected styleUrls
+  styleUrls: ["./product-list.component.css"],
 })
-export class ProductListComponent {
-  products = [
-    {
-      id: 1,
-      name: "Macbook Air",
-      description: "8gb unified memory",
-      price: 13999.99,
-    },
-    {
-      id: 1,
-      name: "Macbook Air",
-      description: "8gb unified memory",
-      price: 13999.99,
-    },
-    {
-      id: 1,
-      name: "Macbook Air",
-      description: "8gb unified memory",
-      price: 13999.99,
-    },
-    {
-      id: 1,
-      name: "Macbook Air",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia per morbi orci vivamus nullam, natoque cursus a himenaeos habitant sociis lobortis id diam facilisi sed. Suscipit urna himenaeos litora aliquet erat convallis neque accumsan, ridiculus imperdiet vivamus quis at sagittis class. Faucibus imperdiet laoreet proin mattis ultrices felis conubia ante, sociis ad iaculis praesent urna dignissim viverra non, vehicula pharetra eu interdum taciti potenti tellus.",
-      price: 13999.99,
-    },
-    {
-      id: 1,
-      name: "Macbook Air",
-      description: "8gb unified memory",
-      price: 13999.99,
-    },
-    {
-      id: 1,
-      name: "Macbook Air",
-      description: "8gb unified memory",
-      price: 13999.99,
-    },
-    {
-      id: 1,
-      name: "Macbook Air",
-      description: "8gb unified memory",
-      price: 13999.99,
-    },
-    {
-      id: 1,
-      name: "Macbook Air",
-      description: "8gb unified memory",
-      price: 13999.99,
-    },
-    {
-      id: 1,
-      name: "Macbook Air",
-      description: "8gb unified memory",
-      price: 13999.99,
-    },
-    {
-      id: 1,
-      name: "Macbook Air",
-      description: "8gb unified memory",
-      price: 13999.99,
-    },
-    {
-      id: 1,
-      name: "Macbook Air",
-      description: "8gb unified memory",
-      price: 13999.99,
-    },
-  ];
-  fetchProduct(id: number): Product | null {
-    return this.products.find((p) => p.id === id) || null;
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+  loading: boolean = true;
+  error: string | null = null;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.fetchProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error("Error fetching products:", err);
+        this.error = "Failed to load products. Error:" + err.message;
+        this.loading = false;
+      },
+      complete: () => {
+        console.log("Products fetch completed");
+      },
+    });
   }
 }
